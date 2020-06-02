e06b315f0ddb48b8bd9d9cdf3b5a2ca692a30ede
Rony Chandra Kudus <51890752+blogcacanid@users.noreply.github.com>
2020-06-02 07:34:52 +0700

    Update Indonesia.jsx

--- src/components/Indonesia.jsx
+++ src/components/Indonesia.jsx
@@ -11,12 +11,12 @@ class Indonesia extends Component {
         };
     }
 
-  componentDidMount() {
-     this.getSummary()
-     this.getDetails()
-  }
+    componentDidMount() {
+        this.getSummary()
+        this.getDetails()
+    }
 
-  getSummary = () => {
+    getSummary = () => {
         fetch("https://api.kawalcorona.com/indonesia/")
         .then( response => response.json())
         .then(
@@ -36,9 +36,9 @@ class Indonesia extends Component {
                 })
             },
         )
-}
+    }
 
-  getDetails = () => {
+    getDetails = () => {
         fetch("https://api.kawalcorona.com/indonesia/provinsi/")
         .then( response => response.json())
         .then(
@@ -49,7 +49,6 @@ class Indonesia extends Component {
                     details : result
                 });
             },
-
             // Handle error
             (error) => {
                 this.setState({
@@ -58,87 +57,75 @@ class Indonesia extends Component {
                 })
             },
         )
-}
+    }
 
-  render() {
+    render() {
         const {error, isLoaded, summaries, details} = this.state;
         if(error){
             return <div>Error in loading</div>
         }else if (!isLoaded) {
             return <div>Loading ...</div>
         }else{
-    return (
-        <div id="content" className="p-4 p-md-5">
-            <h2 className="mb-4">Indonesia</h2>
-                    {
-                        summaries.map(summary => (
-            <div key={summary.id} className="row">
-                <div className="col-md-4 col-sm-6 col-xs-12">
-                    <div className="info-box">
-                        <span className="info-box-icon bg-yellow"><i class="fa fa-group"></i></span>
+            return (
+                <div id="content" className="p-4 p-md-5">
+                    <h2 className="mb-4">Indonesia</h2>
+                    { summaries.map(summary => (
+                        <div key={summary.id} className="row">
+                            <div className="col-md-4 col-sm-6 col-xs-12">
+                                <div className="info-box">
+                                    <span className="info-box-icon bg-yellow"><i class="fa fa-group"></i></span>
 
-                        <div className="info-box-content">
-                            <span className="info-box-text">POSITIF</span>
-                            <span className="info-box-number">{summary.positif}</span>
+                                    <div className="info-box-content">
+                                        <span className="info-box-text">POSITIF</span>
+                                        <span className="info-box-number">{summary.positif}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-6 col-xs-12">
+                                <div className="info-box">
+                                    <span className="info-box-icon bg-green"><i class="fa fa-smile-o"></i></span>
+                                    <div className="info-box-content">
+                                        <span className="info-box-text">SEMBUH</span>
+                                        <span className="info-box-number">{summary.sembuh}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-6 col-xs-12">
+                                <div className="info-box">
+                                    <span className="info-box-icon bg-red"><i class="fa fa-ambulance"></i></span>
+                                    <div className="info-box-content">
+                                        <span className="info-box-text">MENINGGAL</span>
+                                        <span className="info-box-number">{summary.meninggal}</span>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    ))}
+                    <h4>Sistem Informasi Penyebaran Corona (Covid-19) - Indonesia</h4>
+                    <table id="details" className="table table-bordered table-striped">
+                        <thead className="thead-dark">
+                          <tr>
+                            <th scope="col">Propinsi</th>
+                            <th scope="col">Positif</th>
+                            <th scope="col">Sembuh</th>
+                            <th scope="col">Meninggal</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            { details.map(detail => (
+                                <tr key={detail.id}>
+                                    <th scope="row">{detail.attributes.Provinsi}</th>
+                                    <td>{detail.attributes.Kasus_Posi}</td>
+                                    <td>{detail.attributes.Kasus_Semb}</td>
+                                    <td>{detail.attributes.Kasus_Meni}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                      </table>
                 </div>
-                <div className="col-md-4 col-sm-6 col-xs-12">
-                    <div className="info-box">
-                        <span className="info-box-icon bg-green"><i class="fa fa-smile-o"></i></span>
-                        <div className="info-box-content">
-                            <span className="info-box-text">SEMBUH</span>
-                            <span className="info-box-number">{summary.sembuh}</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div className="info-box">
-                        <span className="info-box-icon bg-red"><i class="fa fa-ambulance"></i></span>
-                        <div className="info-box-content">
-                            <span className="info-box-text">MENINGGAL</span>
-                            <span className="info-box-number">{summary.meninggal}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-                        ))
-                    }
-
-
-
-                <h4>Sistem Informasi Penyebaran Corona (Covid-19) - Indonesia</h4>
-<table id="details" className="table table-bordered table-striped">
-  <thead className="thead-dark">
-    <tr>
-      <th scope="col">Propinsi</th>
-      <th scope="col">Positif</th>
-      <th scope="col">Sembuh</th>
-      <th scope="col">Meninggal</th>
-    </tr>
-  </thead>
-  <tbody>
-                    {
-                        details.map(detail => (
-    <tr key={detail.id}>
-      <th scope="row">{detail.attributes.Provinsi}</th>
-      <td>{detail.attributes.Kasus_Posi}</td>
-      <td>{detail.attributes.Kasus_Semb}</td>
-      <td>{detail.attributes.Kasus_Meni}</td>
-    </tr>
-                        ))
-                    }
-
-  </tbody>
-</table>
-
-        </div>
-
-
-
-    );
+            );
+        }
     }
-  }
 }
 
 export default Indonesia;
