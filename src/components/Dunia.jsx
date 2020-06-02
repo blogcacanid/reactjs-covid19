ebbeb376ab3b9cf4e00e27320e753f1ef90e8a37
Rony Chandra Kudus <51890752+blogcacanid@users.noreply.github.com>
2020-06-02 07:36:30 +0700

    Update Dunia.jsx

--- src/components/Dunia.jsx
+++ src/components/Dunia.jsx
@@ -1,38 +1,32 @@
 import React, { Component } from 'react';
 
-class Dunia extends Component {
+class Indonesia extends Component {
     constructor(props){
         super(props);
         this.state = {
             error : null,
             isLoaded : false,
-            sumPositif : [],
-            sumSembuh : [],
-            sumMeninggal : [],
             summaries : [],
             details : []
         };
     }
 
-  componentDidMount() {
-     this.getSumPositif()
-     this.getSumSembuh()
-     this.getSumMeninggal()
-     this.getDetails()
-  }
+    componentDidMount() {
+        this.getSummary()
+        this.getDetails()
+    }
 
-    getSumPositif = () => {
-        fetch("https://api.kawalcorona.com/positif/")
+    getSummary = () => {
+        fetch("https://api.kawalcorona.com/indonesia/")
         .then( response => response.json())
         .then(
             // handle the result
-            (positif) => {
+            (result) => {
                 this.setState({
                     isLoaded : true,
-                    sumPositif : positif
+                    summaries : result
                 });
             },
-
             // Handle error
             (error) => {
                 this.setState({
@@ -43,52 +37,8 @@ class Dunia extends Component {
         )
     }
 
-    getSumSembuh = () => {
-        fetch("https://api.kawalcorona.com/sembuh/")
-        .then( response => response.json())
-        .then(
-            // handle the result
-            (sembuh) => {
-                this.setState({
-                    isLoaded : true,
-                    sumSembuh : sembuh
-                });
-            },
-
-            // Handle error
-            (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                })
-            },
-        )
-    }
-
-    getSumMeninggal = () => {
-        fetch("https://api.kawalcorona.com/meninggal/")
-        .then( response => response.json())
-        .then(
-            // handle the result
-            (meninggal) => {
-                this.setState({
-                    isLoaded : true,
-                    sumMeninggal : meninggal
-                });
-            },
-
-            // Handle error
-            (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                })
-            },
-        )
-    }
-
-  getDetails = () => {
-        fetch("https://api.kawalcorona.com/")
+    getDetails = () => {
+        fetch("https://api.kawalcorona.com/indonesia/provinsi/")
         .then( response => response.json())
         .then(
             // handle the result
@@ -98,7 +48,6 @@ class Dunia extends Component {
                     details : result
                 });
             },
-
             // Handle error
             (error) => {
                 this.setState({
@@ -107,88 +56,74 @@ class Dunia extends Component {
                 })
             },
         )
-}
+    }
 
-  render() {
-        const {error, isLoaded, sumPositif, sumSembuh, sumMeninggal, details} = this.state;
+    render() {
+        const {error, isLoaded, summaries, details} = this.state;
         if(error){
             return <div>Error in loading</div>
         }else if (!isLoaded) {
             return <div>Loading ...</div>
         }else{
-    return (
-        <div id="content" className="p-4 p-md-5">
-            <h2 className="mb-4">Dunia</h2>
-
-
-
-            <div className="row">
-                <div className="col-md-4 col-sm-6 col-xs-12">
-                    <div className="info-box">
-                        <span className="info-box-icon bg-yellow"><i class="fa fa-group"></i></span>
-
-                        <div className="info-box-content">
-                            <span className="info-box-text">CONFIRMED</span>
-                            <span className="info-box-number">{sumPositif['value']}</span>
+            return (
+                <div id="content" className="p-4 p-md-5">
+                    <h2 className="mb-4">Indonesia</h2>
+                    { summaries.map(summary => (
+                        <div key={summary.id} className="row">
+                            <div className="col-md-4 col-sm-6 col-xs-12">
+                                <div className="info-box">
+                                    <span className="info-box-icon bg-yellow"><i class="fa fa-group"></i></span>
+                                    <div className="info-box-content">
+                                        <span className="info-box-text">POSITIF</span>
+                                        <span className="info-box-number">{summary.positif}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4 col-sm-6 col-xs-12">
+                                <div className="info-box">
+                                    <span className="info-box-icon bg-green"><i class="fa fa-smile-o"></i></span>
+                                    <div className="info-box-content">
+                                        <span className="info-box-text">SEMBUH</span>
+                                        <span className="info-box-number">{summary.sembuh}</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-4 col-sm-6 col-xs-12">
+                                <div className="info-box">
+                                    <span className="info-box-icon bg-red"><i class="fa fa-ambulance"></i></span>
+                                    <div className="info-box-content">
+                                        <span className="info-box-text">MENINGGAL</span>
+                                        <span className="info-box-number">{summary.meninggal}</span>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    ))}
+                    <h4>Sistem Informasi Penyebaran Corona (Covid-19) - Indonesia</h4>
+                    <table id="details" className="table table-bordered table-striped">
+                        <thead className="thead-dark">
+                            <tr>
+                                <th scope="col">Propinsi</th>
+                                <th scope="col">Positif</th>
+                                <th scope="col">Sembuh</th>
+                                <th scope="col">Meninggal</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { details.map(detail => (
+                                <tr key={detail.id}>
+                                    <th scope="row">{detail.attributes.Provinsi}</th>
+                                    <td>{detail.attributes.Kasus_Posi}</td>
+                                    <td>{detail.attributes.Kasus_Semb}</td>
+                                    <td>{detail.attributes.Kasus_Meni}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                      </table>
                 </div>
-                <div className="col-md-4 col-sm-6 col-xs-12">
-                    <div className="info-box">
-                        <span className="info-box-icon bg-green"><i class="fa fa-smile-o"></i></span>
-                        <div className="info-box-content">
-                            <span className="info-box-text">RECOVERED</span>
-                            <span className="info-box-number">{sumSembuh['value']}</span>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div className="info-box">
-                        <span className="info-box-icon bg-red"><i class="fa fa-ambulance"></i></span>
-                        <div className="info-box-content">
-                            <span className="info-box-text">DEATHS</span>
-                            <span className="info-box-number">{sumMeninggal['value']}</span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-
-                <h4>Sistem Informasi Penyebaran Corona (Covid-19) - Dunia</h4>
-<table id="details" className="table table-bordered table-striped">
-  <thead className="thead-dark">
-    <tr>
-      <th scope="col">Country</th>
-      <th scope="col">Confirmed</th>
-      <th scope="col">Deaths</th>
-      <th scope="col">Recovered</th>
-      <th scope="col">Active</th>
-    </tr>
-  </thead>
-  <tbody>
-                    {
-                        details.map(detail => (
-    <tr key={detail.id}>
-      <th scope="row">{detail.attributes.Country_Region}</th>
-      <td>{detail.attributes.Confirmed}</td>
-      <td>{detail.attributes.Deaths}</td>
-      <td>{detail.attributes.Recovered}</td>
-      <td>{detail.attributes.Active}</td>
-    </tr>
-                        ))
-                    }
-
-  </tbody>
-</table>
-
-        </div>
-
-
-
-    );
+            );
+        }
     }
-  }
 }
 
-export default Dunia;
+export default Indonesia;
